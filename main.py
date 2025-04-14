2200dc91f5687319792a69d2962b03462b951ce4
Farzad Naderi <farzad.naderi@iis.fraunhofer.de>
2025-04-15 00:00:24 +0200

    feat: add argument parser
    add centralized logging

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
+import argparse
+from logging_config import setup_logging
 from etl.extract import extract_data
 from etl.transform import transform_data
 from etl.load import load_data
 from etl.utils import load_config
+import logging
 
-def main() -> None:
+setup_logging() 
+logger = logging.getLogger(__name__)
+
+def main(config_path: str = "config/config.yaml") -> None:
     """
     main ETL pipeline runner.
     it loads configuration and runs extract, transform, and load steps.
     """
-    config = load_config("config/config.yaml")
+    config = load_config(config_path)
     df = extract_data(config["data"])
     df_clean = transform_data(df, config["transform"])
     load_data(df_clean, config["load"])
+    logger.info("ETL pipeline completed successfully.")
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Run ETL pipeline")
+    parser.add_argument(
+        "--config", 
+        type=str, 
+        default="config/config.yaml", 
+        help="Path to the YAML config file"
+    )
+    args = parser.parse_args()
+    main(args.config)
